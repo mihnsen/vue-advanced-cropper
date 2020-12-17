3952fe81c805762fbf09d7987b045b9dc1afac81
Norserium <norserium@yandex.ru>
2020-12-17 23:30:06 +0300

    Adapt the code to the breaking changes of Vue 3

--- src/Cropper.vue
+++ src/Cropper.vue
@@ -119,11 +119,9 @@ export default {
 		},
 		defaultPosition: {
 			type: [Function, Object],
-			default: algorithms.defaultPosition,
 		},
 		defaultVisibleArea: {
 			type: [Function, Object],
-			default: algorithms.defaultVisibleArea,
 		},
 		defaultBoundaries: {
 			type: [Function, String],
@@ -533,7 +531,7 @@ export default {
 		window.addEventListener('resize', this.refresh);
 		window.addEventListener('orientationchange', this.refresh);
 	},
-	destroyed() {
+	unmounted() {
 		window.removeEventListener('resize', this.refresh);
 		window.removeEventListener('orientationchange', this.refresh);
 		if (this.imageAttributes.revoke && this.imageAttributes.src) {
@@ -854,11 +852,13 @@ export default {
 					);
 				}
 
+				const defaultPositionAlgorithm = this.defaultPosition || algorithms.defaultPosition;
+
 				const transforms = [
 					defaultSize,
 					({ coordinates }) => ({
-						...(isFunction(this.defaultPosition)
-							? this.defaultPosition({
+						...(isFunction(defaultPositionAlgorithm)
+							? defaultPositionAlgorithm({
 									coordinates,
 									imageSize: this.imageSize,
 									visibleArea: this.visibleArea,
@@ -936,8 +936,10 @@ export default {
 						this.resetCoordinates();
 					}
 
-					this.visibleArea = isFunction(this.defaultVisibleArea)
-						? this.defaultVisibleArea({
+					let algorithm = this.defaultVisibleArea || algorithms.defaultVisibleArea;
+
+					this.visibleArea = isFunction(algorithm)
+						? algorithm({
 								imageSize: this.imageSize,
 								boundaries: this.boundaries,
 								coordinates: this.priority !== 'visible-area' ? this.coordinates : null,
@@ -1313,6 +1315,7 @@ export default {
 			}
 		},
 	},
+	emits: ['change', 'error', 'ready'],
 };
 </script>
 
